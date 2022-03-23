5ba21c79a9714782d9b4fef3607b86600ee80476
ammarhassan <ammar@telehealthmgt.com>
2022-03-23 16:48:41 -0400

    Settings ground for testing kafka

--- kafka_connectors/testconnectors/models.py
+++ kafka_connectors/testconnectors/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db.models import Model
+from django.db.models import CharField, TextField
+
+
+class KafkaMessages(Model):
+    key = CharField(help_text='Key', max_length=255, blank=True, null=True)
+    text = TextField(help_text='Text')
